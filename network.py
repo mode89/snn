b0ef4fb9ca358bdc34232b45032c4de2eb97b4d0
Mode <mode89@mail.ru>
2017-04-22 17:04:24 +0800

    refactor Network
    Implement class Synapse, construct neurons and syanpses at Network
    constructor.

--- network.py
+++ network.py
@@ -1,24 +1,23 @@
 import random
+import weakref
+
+class Synapse:
+
+    def output(self):
+        return self.weight * self.pre().output
 
 class Neuron:
 
-    def __init__(self, input_num):
-        self.input_num = input_num
-        self.potential = random.random()
-        self.weight = [random.random() / input_num
-            for i in range(input_num)]
-        self.output = 0.0
-
-    def integrate(self, neurons):
-        for i in range(len(neurons)):
-            self.potential += self.weight[i] * neurons[i].output
+    def integrate(self):
+        for synapse in self.synapses:
+            self.potential += synapse.output()
 
     def fire(self):
         if self.potential > 1.0:
             self.potential = 0.0
             self.output = 1.0
         else:
-            self.output = random.uniform(0.0, 1 / self.input_num)
+            self.output = random.uniform(0.0, 1 / len(self.synapses))
 
     def spiking(self):
         return self.output == 1.0
@@ -26,7 +25,15 @@ class Neuron:
 class Network:
 
     def __init__(self, neuron_num):
-        self.neurons = [Neuron(neuron_num) for i in range(neuron_num)]
+        self.neurons = [Neuron() for i in range(neuron_num)]
+        for neuron in self.neurons:
+            neuron.potential = random.random()
+            neuron.output = 0.0
+            neuron.synapses = [Synapse() for i in range(neuron_num)]
+            for synapse, pre_neuron in zip(neuron.synapses, self.neurons):
+                synapse.pre = weakref.ref(pre_neuron)
+                synapse.post = neuron
+                synapse.weight = random.random() / neuron_num
 
     def step(self):
         self.integrate()
@@ -34,7 +41,7 @@ class Network:
 
     def integrate(self):
         for neuron in self.neurons:
-            neuron.integrate(self.neurons)
+            neuron.integrate()
 
     def fire(self):
         for neuron in self.neurons:
