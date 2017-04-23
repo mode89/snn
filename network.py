837d55f60214fb71d61578e9562973a3fc9db527
Mode <mode89@mail.ru>
2017-04-23 21:08:53 +0800

    implement STDP

--- network.py
+++ network.py
@@ -6,20 +6,42 @@ class Synapse:
     def output(self):
         return self.weight * self.pre().output
 
+    def update_weight(self):
+        if self.pre() is not self.post():
+            if self.post().fired():
+                self.weight += self.pre().spike_trace * 0.01
+            elif self.pre().fired():
+                self.weight -= self.post().spike_trace * 0.01
+
 class Neuron:
+
+    def __init__(self):
+        self.spike_trace = 0.0
 
     def integrate(self):
         for synapse in self.synapses:
             self.potential += synapse.output()
+        self.update_spike_trace()
+
+    def update_spike_trace(self):
+        if self.spike_trace > 0.0:
+            self.spike_trace -= 0.1
+        else:
+            self.spike_trace = 0.0
 
     def fire(self):
         if self.potential > 1.0:
             self.potential = 0.0
             self.output = 1.0
+            self.spike_trace = 1.0
         else:
             self.output = random.uniform(0.0, 1 / len(self.synapses))
 
-    def spiking(self):
+    def update_weights(self):
+        for synapse in self.synapses:
+            synapse.update_weight()
+
+    def fired(self):
         return self.output == 1.0
 
 class Network:
@@ -32,12 +54,13 @@ class Network:
             neuron.synapses = [Synapse() for i in range(neuron_num)]
             for synapse, pre_neuron in zip(neuron.synapses, self.neurons):
                 synapse.pre = weakref.ref(pre_neuron)
-                synapse.post = neuron
+                synapse.post = weakref.ref(neuron)
                 synapse.weight = random.random() / neuron_num
 
     def step(self):
         self.integrate()
         self.fire()
+        self.update_weights()
 
     def integrate(self):
         for neuron in self.neurons:
@@ -46,6 +69,10 @@ class Network:
     def fire(self):
         for neuron in self.neurons:
             neuron.fire()
+
+    def update_weights(self):
+        for neuron in self.neurons:
+            neuron.update_weights()
 
 class Monitor:
 
@@ -58,7 +85,7 @@ class Monitor:
     def step(self):
         for i in range(len(self.network.neurons)):
             neuron = self.network.neurons[i]
-            if neuron.spiking():
+            if neuron.fired():
                 self.x.append(self.counter)
                 self.y.append(i)
         self.counter += 1
