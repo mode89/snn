d0131ce95bad2c3b68ee47e5dae0e4b7e831fa1f
Mode <mode89@mail.ru>
2017-04-28 14:25:01 +0800

    add comments

--- izhikevich/stdp.py
+++ izhikevich/stdp.py
@@ -21,17 +21,20 @@ d = numpy.concatenate((
     2 * numpy.ones(Ni)
 ))
 
+# generate post-synaptic connections
 post = numpy.empty((N, M), dtype=numpy.int)
 for i in range(Ne):
     post[i,:] = random.sample(range(N), M)
 for i in range(Ne, N):
     post[i,:] = random.sample(range(Ne), M)
 
+# find pre-synaptic connections to excitatory neurons
 pre = [[] for i in range(N)]
 for i in range(Ne):
     for j in range(M):
         pre[post[i,j]].append(i)
 
+# generate delays
 delays = [[[] for i in range(D)] for j in range(N)]
 for i in range(Ne):
     for j in range(M):
@@ -40,6 +43,7 @@ for i in range(Ne, N):
     for j in range(M):
         delays[i][0].append(post[i,j])
 
+# generate matrix of synaptic weights
 s = numpy.zeros((N, N))
 for i in range(Ne):
     s[i, post[i,:]] = 6.0
@@ -56,14 +60,17 @@ STDP = numpy.zeros(N)
 for t in range(T):
     print(t)
 
+    # provide random input
     I = numpy.zeros(N)
     I[int(N * random.random())] = 20.0
 
+    # identify fired neurons
     fired = numpy.argwhere(v >= 30)
     if fired.size > 0:
         v[fired] = -65.0
         u[fired] = u[fired] + d[fired]
 
+    # deliver spikes to post-synaptic neurons
     firings.append(fired)
     for time in range(min(D, len(firings))):
         for fired_neuron in firings[t - time]:
@@ -71,10 +78,12 @@ for t in range(T):
             if len(post_neurons) > 0:
                 I[post_neurons] += s[fired_neuron, post_neurons]
 
+    # update post-synaptic potential
     for i in range(2):
         v += 0.5 * ((0.04 * v + 5.0) * v + 140.0 - u + I)
     u += a * (0.2 * v - u)
 
+    # update synaptic weights
     STDP[fired] = 1.0
     for fired_neuron in fired:
         if fired_neuron < Ne:
