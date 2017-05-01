3bd5ae503342cf8b45e9969c0c833f6f28bcc3ff
Mode <mode89@mail.ru>
2017-05-01 17:00:43 +0800

    implement classical conditioning

--- izhikevich/classical_conditioning.py
+++ izhikevich/classical_conditioning.py
@@ -1,16 +1,17 @@
+import collections
 import matplotlib.pyplot as plt
 import numpy
 import random
 
 random.seed(0)
 
-T = 100000
+T = 3600 * 10
 N = 1000
 Ne = int(N * 0.8)
 Ni = N - Ne
 
 M = int(N * 0.1)
-D = 20
+D = 1
 
 a = numpy.concatenate((
     0.02 * numpy.ones(Ne),
@@ -46,14 +47,14 @@ for i in range(Ne, N):
 # generate matrix of synaptic weights
 s = numpy.zeros((N, N))
 for i in range(Ne):
-    s[i, post[i,:]] = 6.0
+    s[i, post[i,:]] = 1.0
 for i in range(Ne, N):
-    s[i, post[i,:]] = -5.0
+    s[i, post[i,:]] = -1.0
 
 v = -65 * numpy.ones(N)
 u = 0.2 * v
 
-firings = []
+firings = collections.deque(maxlen=D)
 
 STDP = numpy.zeros(N)
 sm = 4.0
@@ -61,103 +62,131 @@ sd = numpy.zeros((N, N))
 
 DA = 0
 rew = []
-n1 = 700
-n2 = post[n1, 0]
-s[n1, n2] = 0.0
 
-interval = 20
-n1f = []
-n2f = []
+ST = 100
+stimulus = [[] for i in range(ST)]
+for i in range(ST):
+    stimulus[i] = random.sample(range(N), 50)
+next_stimuli = 0
+s_mean_indices = [i for i in range(N) if i not in stimulus[0]]
 
 class data:
-    n0 = 700
-    n1 = post[n0, 0]
-    stdp0 = []
-    stdp1 = []
-    s01 = []
-    da = []
+    firings = []
+    stimulus = []
+    s0_mean = []
+    s_mean = []
 
-for t in range(T):
-    print(t)
+plt.subplot(211)
+scatter = plt.scatter([], [], color="black", marker=".")
 
-    # provide random input
-    I = 13.0 * numpy.array([random.uniform(-0.5, 0.5) for i in range(N)])
+plt.subplot(212)
+s_mean_line, s0_mean_line = plt.plot([], [], [], [])
 
-    # identify fired neurons
-    fired = numpy.argwhere(v >= 30)
-    if fired.size > 0:
-        v[fired] = -65.0
-        u[fired] = u[fired] + d[fired]
+for sec in range(T):
 
-    # deliver spikes to post-synaptic neurons
-    firings.append(fired)
-    for time in range(min(D, len(firings))):
-        for fired_neuron in firings[t - time]:
-            post_neurons = delays[fired_neuron][time]
-            if len(post_neurons) > 0:
-                I[post_neurons] += s[fired_neuron, post_neurons]
+    data.firings = []
+    data.stimulus = []
 
-    # update post-synaptic potential
-    for i in range(2):
-        v += 0.5 * ((0.04 * v + 5.0) * v + 140.0 - u + I)
-    u += a * (0.2 * v - u)
+    for ms in range(1000):
+        t = sec * 1000 + ms
+        print(t)
 
-    # update synaptic weights
-    STDP[fired] = 0.1
-    for fired_neuron in fired:
-        if fired_neuron < Ne:
-            post_neurons = post[fired_neuron,:]
-            sd[fired_neuron, post_neurons] -= 1.2 * STDP[post_neurons]
-        pre_neurons = pre[fired_neuron]
-        sd[pre_neurons, fired_neuron] += STDP[pre_neurons]
-    STDP *= 0.95
+        # provide random input
+        I = 13.0 * numpy.array(
+            [random.uniform(-0.5, 0.5) for i in range(N)])
 
-    DA *= 0.995
-    if t % 10 == 0:
-        s[0:Ne,:] = numpy.maximum(0.0, numpy.minimum(sm,
-            s[0:Ne,:] + (0.002 + DA) * sd[0:Ne,:]))
-        sd *= 0.99
-    if numpy.any(fired == n1):
-        n1f.append(t)
-    if numpy.any(fired == n2):
-        n2f.append(t)
-        if len(n1f) > 0:
-            if t - n1f[-1] < interval and n2f[-1] > n1f[-1]:
-                print("Coincident spiking")
-                rew.append(t + 1000 + int(2000 * random.random()))
-    if any([it == t for it in rew]):
-        print("Rewarding")
-        DA += 0.5
+        # issuse random stimuli
+        if t == next_stimuli:
+            print("Stimuli")
+            next_stimuli = t + random.randint(100, 300)
+            stimuli = random.randrange(0, ST)
+            neurons = stimulus[stimuli]
+            I[neurons] = 17.0
 
-    data.stdp0.append(STDP[data.n0])
-    data.stdp1.append(STDP[data.n1])
-    data.s01.append(s[data.n0, data.n1])
-    data.da.append(DA)
+            # reward stimuli with index 0
+            if stimuli == 0:
+                print("Reward")
+                rew.append(t + random.randrange(0, 1000))
+                data.stimulus.append(t)
 
-x = []
-y = []
-for t in range(T):
-    for fired in firings[t]:
-        x.append(t)
-        y.append(fired)
+        # identify fired neurons
+        fired = numpy.argwhere(v >= 30)
+        if fired.size > 0:
+            v[fired] = -65.0
+            u[fired] = u[fired] + d[fired]
 
-plt.subplot(411)
-plt.scatter(x, y, color="black", marker=".")
-plt.xlim(0, T)
-plt.ylim(0, N)
+        # deliver spikes to post-synaptic neurons
+        firings.appendleft(fired)
+        for time in range(len(firings)):
+            for fired_neuron in firings[time]:
+                post_neurons = delays[fired_neuron][time]
+                if len(post_neurons) > 0:
+                    I[post_neurons] += s[fired_neuron, post_neurons]
 
-plt.subplot(412)
-plt.plot(
-    range(T), data.stdp0,
-    range(T), data.stdp1)
-plt.xlim(0, T)
+        # update post-synaptic potential
+        for i in range(2):
+            v += 0.5 * ((0.04 * v + 5.0) * v + 140.0 - u + I)
+        u += a * (0.2 * v - u)
 
-plt.subplot(413)
-plt.plot(range(T), data.s01)
-plt.xlim(0, T)
+        # update eligibility trace
+        STDP[fired] = 0.1
+        for fired_neuron in fired:
+            if fired_neuron < Ne:
+                post_neurons = post[fired_neuron,:]
+                sd[fired_neuron, post_neurons] -= 1.2 * STDP[post_neurons]
+            pre_neurons = pre[fired_neuron]
+            sd[pre_neurons, fired_neuron] += STDP[pre_neurons]
+        STDP *= 0.95
 
-plt.subplot(414)
-plt.plot(range(T), data.da)
-plt.xlim(0, T)
+        # update synaptics weights
+        DA *= 0.995
+        if t % 10 == 0:
+            s[0:Ne,:] = numpy.maximum(0.0, numpy.minimum(sm,
+                s[0:Ne,:] + (0.002 + DA) * sd[0:Ne,:]))
+            sd *= 0.99
 
-plt.show()
+        # issue reward
+        if any([it == t for it in rew]):
+            print("Increase dopamine")
+            DA += 0.5
+
+        # save plotting data
+        data.firings.append(fired)
+        data.s_mean.append(
+            numpy.sum(s[s_mean_indices,0:Ne]) / ((N - 50) * M))
+        data.s0_mean.append(
+            numpy.sum(s[stimulus[0],0:Ne]) / (50 * M))
+
+    # Update plots
+
+    plt.subplot(211)
+
+    points = []
+    for ms in range(1000):
+        t = sec * 1000 + ms
+        for fired in data.firings[ms]:
+            points.append((t, fired))
+
+    # plot spikes
+    scatter.set_offsets(points)
+    plt.xlim(sec * 1000, sec * 1000 + 1000)
+    plt.ylim(0, N)
+
+    # plot rewarded stimulus
+    for st in data.stimulus:
+        plt.axvline(st, color="red")
+
+    # plot mean value of synaptic strength
+    s_mean_ax = plt.subplot(212)
+    s_mean_line.set_data(
+        range(sec * 1000 + 1000), data.s_mean)
+    s0_mean_line.set_data(
+        range(sec * 1000 + 1000), data.s0_mean)
+
+    s_mean_ax.relim()
+    s_mean_ax.set_xlim(0, sec * 1000 + 1000)
+    s_mean_ax.autoscale_view()
+
+    # update UI
+    plt.ion()
+    plt.pause(0.01)
