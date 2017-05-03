ecb2d407edf71a810e64a9d11643a53ffba9fafa
Mode <mode89@mail.ru>
2017-05-03 23:44:08 +0800

    initialize numpy api

--- snn.cpp
+++ snn.cpp
@@ -1,5 +1,22 @@
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+
 #include <boost/python.hpp>
 #include <iostream>
+#include <numpy/arrayobject.h>
+
+#undef NUMPY_IMPORT_ARRAY_RETVAL
+#define NUMPY_IMPORT_ARRAY_RETVAL
+
+class NumpyInitializer
+{
+public:
+    NumpyInitializer()
+    {
+        import_array();
+    }
+};
+
+static NumpyInitializer sNumpyInitializer;
 
 void greet()
 {
